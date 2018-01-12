6ebba30eb20bdff96452fcb80dd2fa0a8068a2a1
kf <k@enemyspeak.com>
2018-01-12 02:21:22 -0600

    app styles

--- app/components/user-list.js
+++ app/components/user-list.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   classNames: ['user-list-contain'],
   classNameBindings: ["isExpired:is-expired"],
+  sortType: 'Newest First',
   //  go to api
   model: [
       {
