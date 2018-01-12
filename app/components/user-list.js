394601c2ecff49d6657f67c59ea62f044b24041f
kf <k@enemyspeak.com>
2018-01-12 03:36:49 -0600

    chat styling

--- app/components/user-list.js
+++ app/components/user-list.js
@@ -2,15 +2,31 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['user-list-contain'],
-  classNameBindings: ["isExpired:is-expired"],
   sortType: 'Newest First',
   //  go to api
   model: [
-      {
-        name: 'hello'
-      },{
-        name: 'cool coolname'
-      }
-    ]
-  
+    {
+      name: 'Cool Coolname',
+      relativeTime: '11:45 pm',
+      type: 'Private Message',
+      friend: true,
+      snippet: 'Hey dude Hope you are fine lorem ipsum Dolor sit amet, consectetur adipiscing elit, sed do magnaet ifsa nidsian kgskhg idshgi ingskngls klngds'
+    },
+    {
+      name: 'Cool Coolname',
+      relativeTime: '11:45 pm',
+      type: 'Private Message',
+      friend: false,
+      bookmark: true,
+      snippet: 'Hey dude Hope you are fine lorem ipsum Dolor sit amet, consectetur adipiscing elit, sed do magnaet ifsa nidsian kgskhg idshgi ingskngls klngds'
+    },
+    {
+      name: 'Cool Coolname',
+      relativeTime: '11:45 pm',
+      type: 'Private Message',
+      selected: true,
+      friend: false,
+      snippet: 'Hey dude Hope you are fine lorem ipsum Dolor sit amet, consectetur adipiscing elit, sed do magnaet ifsa nidsian kgskhg idshgi ingskngls klngds'
+    }
+  ],
 });
--- app/components/user-object.js
+++ app/components/user-object.js
@@ -1,5 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['user-object'],
+	classNames: ['user-object'],
+	classNameBindings: ["isSelected:selected"],
+	
+	user: null,
+	
+	isSelected: Ember.computed("user.selected", function() {
+		let user = this.get("user");
+
+		if ( user.selected ) {
+			return true;
+		}
+		return false;
+	})
 });
